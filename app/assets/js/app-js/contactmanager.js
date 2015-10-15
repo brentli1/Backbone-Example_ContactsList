c85fcaba3381a9bcf55e8b5c0e29d18b50df44ca
brentli1 <brent.linville@roundingwell.com>
2015-10-15 14:51:19 -0500

    Update package.json

---
+++ app/assets/js/app-js/contactmanager.js
@@ -0,0 +1,14 @@
+window.ContactManager = {
+	Models: {},
+	Collections: {},
+	Views: {},
+
+	start: function() {
+		console.log('started');
+	}
+};
+
+$(function() {
+	ContactManager.start();
+});
+
