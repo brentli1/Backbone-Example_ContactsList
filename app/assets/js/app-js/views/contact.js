04d653032d780eecc8e9a8c5fb70dde30c4c8cc9
brentli1 <brent.linville@roundingwell.com>
2015-10-16 12:37:55 -0500

    updates
    - added header
    - added remove contact functionality

--- app/assets/js/app-js/views/contact.js
+++ app/assets/js/app-js/views/contact.js
@@ -3,6 +3,19 @@ ContactManager.Views.Contact = Backbone.View.extend({
 	className: 'contact--wrap',
 	template: ContactManager.templates.contact,
 
+    events: {
+        'click .js-delete-contact':'onClickDelete'
+    },
+
+    initialize: function() {
+        this.listenTo(this.model, 'remove', this.remove);
+    },
+
+    onClickDelete: function(e) {
+        e.preventDefault();
+        this.model.collection.remove(this.model);
+    },
+
 	render: function() {
 	  var html = this.template(this.model.toJSON());
 	  this.$el.append(html);
